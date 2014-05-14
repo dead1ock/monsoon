6292b620253da9071f1b461b58ca536eaaef4e14
Dallin Wellington <dallin.wellington@gmail.com>
2014-05-14 14:20:33 -0700

    Added option for dynamic libraries in create-module.py

--- utils/create-module.py
+++ utils/create-module.py
@@ -13,7 +13,7 @@ if os.path.exists(module_filepath):
 
 print ('Creating Module - ' , module_name)
 
-type = input("Type? [lib, exe]: ")
+type = input("Type? [lib, exe, dynlib]: ")
 
 os.makedirs(module_filepath)
 
@@ -44,18 +44,22 @@ cmakeFile = open(module_filepath + "/CMakeLists.txt", 'a')
 cmakeFile.write("CMAKE_MINIMUM_REQUIRED(VERSION 2.8.12)\n")
 cmakeFile.write("PROJECT(" + module_name + " C CXX)\n\n")
 
-cmakeFile.write("INCLUDE_DIRECTORIES(include/"+ module_name)
-cmakeFile.write("INCLUDE(Sources.cmake)"\n")
+cmakeFile.write("INCLUDE_DIRECTORIES(include/"+ module_name + ")\n")
+cmakeFile.write("INCLUDE(Sources.cmake)\n")
 
 if type == "lib":
-	cmakeFile.write("ADD_LIBRARY(" + module_name + " ${MODULE_SOURCES})\n")
+	cmakeFile.write("ADD_LIBRARY(" + module_name + " STATIC ${MODULE_SOURCES})\n")
 	cmakeFile.write("SET_TARGET_PROPERTIES(" + module_name + " PROPERTIES LINKER_LANGUAGE CXX)\n")
 elif type == "exe":
 	cmakeFile.write("ADD_EXECUTABLE(" + module_name + " ${MODULE_SOURCES})\n");
+elif type == "dynlib":
+	cmakeFile.write("ADD_DEFINITIONS(-DMONSOON_DYNLIB_EXPORT)\n")
+	cmakeFile.write("ADD_LIBRARY(" + module_name + " SHARED ${MODULE_SOURCES})\n")
+	cmakeFile.write("SET_TARGET_PROPERTIES(" + module_name + " PROPERTIES LINKER_LANGUAGE CXX)\n")
 
 cmakeFile.close()
 
-shutil.copyfile("Sources.cmake.in", "Sources.cmake")
+shutil.copyfile("Sources.cmake.in", module_filepath + "/Sources.cmake")
 
 readmeFile = open(module_filepath + "/README.md", 'a')
 readmeFile.write("#Monsoon Game Engine\n##Module: " + module_name + "\n")
