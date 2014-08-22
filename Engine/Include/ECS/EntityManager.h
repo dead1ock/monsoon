d2156a87fd26c5ab7e01e770e8de4c81de2046e4
Dallin Wellington <dallin.wellington@gmail.com>
2014-08-21 18:14:41 -0700

    [Engine] EntityManager documentation.

--- Engine/Include/ECS/EntityManager.h
+++ Engine/Include/ECS/EntityManager.h
@@ -13,16 +13,39 @@
 
 namespace Monsoon {
 	namespace ECS {
+
+		/**
+		 * Manages the creation, destruction, and lookup of simulation entities.
+		 */
 		class DYNLIB EntityManager
 		{
 		public:
 			EntityManager(Event::EventManager* eventManager);
 			~EntityManager();
 
+			/**
+			 * Creates a new entity and associates it with the sepcified string identifier.
+			 */
 			Entity CreateEntity(std::string identifier);
+
+			/**
+			 * Creates a new entity which has no string identifer (anonymous).
+			 */
 			Entity CreateEntity();
+
+			/**
+			 * Finds an entity which has the target associated string identifier.
+			 */
 			Entity FindEntity(std::string identifier);
+
+			/**
+			 * Destroys an entity based on its associated string identifier.
+			 */
 			void DestroyEntity(std::string identifier);
+
+			/**
+			 * Destroys an entity based on its handle.
+			 */
 			void DestroyEntity(Entity entity);
 
 		private:
