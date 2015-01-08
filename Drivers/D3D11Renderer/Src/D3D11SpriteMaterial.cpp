25738034307c03fa1dad2a66b233b177b4d7ccd0
Dallin Wellington <dallin.wellington@gmail.com>
2015-01-07 23:34:55 -0700

    Set UV addressing mode to CLAMP for sprites.

--- Drivers/D3D11Renderer/Src/D3D11SpriteMaterial.cpp
+++ Drivers/D3D11Renderer/Src/D3D11SpriteMaterial.cpp
@@ -132,9 +132,9 @@ bool D3D11SpriteMaterial::Load(ID3D11Device* device, HWND windowHandle)
 	// Describe and create sampler.
 	//
 	samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
-	samplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
-	samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
-	samplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
+	samplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_CLAMP;
+	samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_CLAMP;
+	samplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_CLAMP;
 	samplerDesc.MipLODBias = 0.0f;
 	samplerDesc.MaxAnisotropy = 1;
 	samplerDesc.ComparisonFunc = D3D11_COMPARISON_ALWAYS;
