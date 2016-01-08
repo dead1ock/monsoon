e12dce7b95083293b5a1cd9b8bdcaca45e58a07e
Dallin Wellington <dallin.wellington@gmail.com>
2016-01-08 11:07:13 -0700

    Added projection, perpendicular, unit functions and additional operators to all Vector classes. Created Vector4 class.

--- Engine/Include/Math/Vector.h
+++ Engine/Include/Math/Vector.h
@@ -24,8 +24,21 @@ namespace Monsoon {
 			Float Magnitude();
 			Float Dot(Vector2& other);
 
+			Vector2 Proj(Vector2& other);
+			Vector2 Perp(Vector2& other);
+			Vector2 Unit();
+
 			Vector2 operator+(Vector2& other); // Vector Addition
 			void operator+=(Vector2& other);
+
+			Vector2 operator-(Vector2& other); // Vector Subtration
+			void operator-=(Vector2& other);
+
+			Vector2 operator*(Float scalar); // Scalar Multiplication
+			void operator*=(Float scalar);
+
+			Vector2 operator/(Float scalar); // Scalar Division
+			void operator/=(Float scalar);
 
 			Float mX, mY;
 		};
@@ -38,12 +51,55 @@ namespace Monsoon {
 
 			Float Magnitude();
 			Vector3 Cross(Vector3& other);
+
+			Vector3 Proj(Vector3& other);
+			Vector3 Perp(Vector3& other);
+			Vector3 Unit();
+			
 			Float Dot(Vector3& other);
 
 			Vector3 operator+(Vector3& other); // Vector Addition
 			void operator+=(Vector3& other);
 
+			Vector3 operator-(Vector3& other); // Vector Subtration
+			void operator-=(Vector3& other);
+
+			Vector3 operator*(Float scalar); // Scalar Multiplication
+			void operator*=(Float scalar);
+
+			Vector3 operator/(Float scalar); // Scalar Division
+			void operator/=(Float scalar);
+
 			Float mX, mY, mZ;
+		};
+
+		class DYNLIB Vector4
+		{
+		public:
+			Vector4(float x, float y, float z, float w);
+			~Vector4();
+
+			Float Magnitude();
+
+			Vector4 Proj(Vector4& other);
+			Vector4 Perp(Vector4& other);
+			Vector4 Unit();
+
+			Float Dot(Vector4& other);
+
+			Vector4 operator+(Vector4& other); // Vector Addition
+			void operator+=(Vector4& other);
+
+			Vector4 operator-(Vector4& other); // Vector Subtration
+			void operator-=(Vector4& other);
+
+			Vector4 operator*(Float scalar); // Scalar Multiplication
+			void operator*=(Float scalar);
+
+			Vector4 operator/(Float scalar); // Scalar Division
+			void operator/=(Float scalar);
+
+			Float mX, mY, mZ, mW;
 		};
 	}
 }
--- Engine/Src/Math/Vector.cpp
+++ Engine/Src/Math/Vector.cpp
@@ -8,6 +8,10 @@
 
 using namespace Monsoon;
 using namespace Monsoon::Math;
+
+//----------------------------------------------------
+// Vector 2
+//----------------------------------------------------
 		
 Vector2::Vector2(float x, float y)
 	: mX(x)
@@ -31,6 +35,21 @@ Float Vector2::Dot(Vector2& other)
 	return ((mX * other.mX) + (mY * other.mY));
 }
 
+Vector2 Vector2::Unit()
+{
+	return (*this / Magnitude());
+}
+
+Vector2 Vector2::Proj(Vector2& other)
+{
+	return other.Unit() * Float(Dot(other) / other.Magnitude());
+}
+
+Vector2 Vector2::Perp(Vector2& other)
+{
+	return (*this - Proj(other));
+}
+
 Vector2 Vector2::operator+(Vector2& other)
 {
 	return Vector2(mX + other.mX, mY + other.mY);
@@ -42,8 +61,41 @@ void Vector2::operator+=(Vector2& other)
 	mY += other.mY;
 }
 
+Vector2 Vector2::operator-(Vector2& other)
+{
+	return Vector2(mX - other.mX, mY - other.mY);
+}
+
+void Vector2::operator-=(Vector2& other)
+{
+	mX -= other.mX;
+	mY -= other.mY;
+}
+
+Vector2 Vector2::operator* (Float scalar)
+{
+	return Vector2(mX * scalar, mY * scalar);
+}
+
+void Vector2::operator*=(Float scalar)
+{
+	mX *= scalar;
+	mY *= scalar;
+}
+
+Vector2 Vector2::operator/ (Float scalar)
+{
+	return Vector2(mX / scalar, mY / scalar);
+}
+
+void Vector2::operator/= (Float scalar)
+{
+	mX /= scalar;
+	mY /= scalar;
+}
+
 //----------------------------------------------------
-//
+// Vector 3
 //----------------------------------------------------
 
 Vector3::Vector3(float x, float y, float z)
@@ -69,6 +121,21 @@ Float Vector3::Dot(Vector3& other)
 	return ((mX * other.mX) + (mY * other.mY) + (mZ * other.mZ));
 }
 
+Vector3 Vector3::Unit()
+{
+	return (*this / Magnitude());
+}
+
+Vector3 Vector3::Proj(Vector3& other)
+{
+	return other.Unit() * Float(Dot(other) / other.Magnitude());
+}
+
+Vector3 Vector3::Perp(Vector3& other)
+{
+	return (*this - Proj(other));
+}
+
 Vector3 Vector3::Cross(Vector3& other)
 {
 	return Vector3((mY*other.mZ) - (mZ * other.mY), (mZ*other.mX) - (mX*other.mZ), (mX*other.mY) - (mY*other.mX));
@@ -84,4 +151,135 @@ void Vector3::operator+=(Vector3& other)
 	mX += other.mX;
 	mY += other.mY;
 	mZ += other.mZ;
+}
+
+Vector3 Vector3::operator-(Vector3& other)
+{
+	return Vector3(mX - other.mX, mY - other.mY, mZ - other.mZ);
+}
+
+void Vector3::operator-=(Vector3& other)
+{
+	mX -= other.mX;
+	mY -= other.mY;
+	mZ -= other.mZ;
+}
+
+Vector3 Vector3::operator* (Float scalar)
+{
+	return Vector3(mX * scalar, mY * scalar, mZ * scalar);
+}
+
+void Vector3::operator*=(Float scalar)
+{
+	mX *= scalar;
+	mY *= scalar;
+	mZ *= scalar;
+}
+
+Vector3 Vector3::operator/ (Float scalar)
+{
+	return Vector3(mX / scalar, mY / scalar, mZ / scalar);
+}
+
+void Vector3::operator/= (Float scalar)
+{
+	mX /= scalar;
+	mY /= scalar;
+	mZ /= scalar;
+}
+
+//----------------------------------------------------
+// Vector 4
+//----------------------------------------------------
+
+Vector4::Vector4(float x, float y, float z, float w)
+	: mX(x)
+	, mY(y)
+	, mZ(z)
+	, mW(w)
+{
+
+}
+
+Vector4::~Vector4()
+{
+
+}
+
+Float Vector4::Magnitude()
+{
+	return sqrt((mX*mX) + (mY*mY) + (mZ*mZ) + (mW*mW));
+}
+
+Float Vector4::Dot(Vector4& other)
+{
+	return ((mX * other.mX) + (mY * other.mY) + (mZ * other.mZ) + (mW * other.mW));
+}
+
+Vector4 Vector4::Unit()
+{
+	return (*this / Magnitude());
+}
+
+Vector4 Vector4::Proj(Vector4& other)
+{
+	return other.Unit() * Float(Dot(other) / other.Magnitude());
+}
+
+Vector4 Vector4::Perp(Vector4& other)
+{
+	return (*this - Proj(other));
+}
+
+Vector4 Vector4::operator+(Vector4& other)
+{
+	return Vector4(mX + other.mX, mY + other.mY, mZ + other.mZ, mW + other.mW);
+}
+
+void Vector4::operator+=(Vector4& other)
+{
+	mX += other.mX;
+	mY += other.mY;
+	mZ += other.mZ;
+	mW += other.mW;
+}
+
+Vector4 Vector4::operator-(Vector4& other)
+{
+	return Vector4(mX - other.mX, mY - other.mY, mZ - other.mZ, mW - other.mW);
+}
+
+void Vector4::operator-=(Vector4& other)
+{
+	mX -= other.mX;
+	mY -= other.mY;
+	mZ -= other.mZ;
+	mW -= other.mW;
+}
+
+Vector4 Vector4::operator* (Float scalar)
+{
+	return Vector4(mX * scalar, mY * scalar, mZ * scalar, mW * scalar);
+}
+
+void Vector4::operator*=(Float scalar)
+{
+	mX *= scalar;
+	mY *= scalar;
+	mZ *= scalar;
+	mW *= scalar;
+}
+
+Vector4 Vector4::operator/ (Float scalar)
+{
+	return Vector4(mX / scalar, mY / scalar, mZ / scalar, mW /scalar);
+}
+
+void Vector4::operator/= (Float scalar)
+{
+	mX /= scalar;
+	mY /= scalar;
+	mZ /= scalar;
+	mW /= scalar;
 }
