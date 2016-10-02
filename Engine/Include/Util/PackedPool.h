515b5201e6daa73144c9d78f8bcf20e3776da326
Dallin Wellington <dallin.wellington@gmail.com>
2016-10-01 21:17:20 -0700

    Packed pool documentation.

--- Engine/Include/Util/PackedPool.h
+++ Engine/Include/Util/PackedPool.h
@@ -18,7 +18,7 @@ namespace Monsoon {
 		* Holds a contiguous pool of objects which can be packed (sorted) for better performance when iterating, 
 		* but accessed via an unchanging unique identifier (for example, a handle or entity id). The goal of this 
 		* container is to pack objects together as close as possible in memory to increase cache coherency when iterating 
-		* over the pool. This comes at a cost of some memory overhead, due to id-to-index bookkeeping.
+		* over the pool. This comes at a cost of significant memory overhead, due to id-to-index bookkeeping.
 		*/
 		template<typename Id, typename Object>
 		class PackedPool
@@ -27,35 +27,56 @@ namespace Monsoon {
 			typedef U16 Index;
 
 		public:
+			/**
+			 * Default constructor.
+			 */
 			PackedPool() {
 				mPackedObjects.reserve(DEFAULT_MAX_POOL_SIZE);
 				mIndexTable.resize(DEFAULT_MAX_POOL_SIZE, USHRT_MAX);
 			}
 
+			/**
+			 * Constructs a packed pool with a maximum size.
+			 *
+			 * @param maxSize The maximum size of the pool.
+			 */
 			PackedPool(U16 maxSize) {
 				mPackedObjects.reserve(maxSize);
 				mIndexTable.resize(maxSize, USHRT_MAX);
 			}
 
+			/**
+			 * Adds an object to the pool.
+			 * Performane: O(1)
+			 * 
+			 * @param id The unique id of the object.
+			 * @param object The object.
+			 */
 			inline void Add(Id id, Object& object) {
 				mIndexTable[id] = (U16)mPackedObjects.size();
 				mPackedObjects.push_back(std::pair<Object, Id>(object, id));
 			}
 
 			/**
-			 * Returns the object from the packed array by INDEX.
+			 * Returns the object from the packed array by INDEX. O(1)
 			 */
 			inline Object& At(Index index) {
 				return mPackedObjects[index].first;
 			}
 
 			/**
-			* Returns the object from the packed array by ID.
+			* Returns the object from the packed array by ID. O(1)
 			*/
 			inline Object& operator[](Id id) {
 				return mPackedObjects[mIndexTable[id]].first;
 			}
 
+			/**
+			 * Removes the object with the associated unique-id from the table.
+			 * Performane: O(1)
+			 *
+			 * @param id The unique-id of the object to remove.
+			 */
 			inline void Remove(Id id) {
 				Index removeIndex = mIndexTable[id];
 				std::pair<Object, Id>& removeObject = mPackedObjects[removeIndex];
@@ -71,16 +92,56 @@ namespace Monsoon {
 				mPackedObjects.pop_back();
 			}
 
+			/**
+			 * @returns The number of objects in the packed pool.
+			 */
 			inline U16 Size() { return (U16) mPackedObjects.size(); }
 
+			/**
+			 * Queries the pool to see if the specified object
+			 * exists in it.
+			 * Performance O(1)
+			 *
+			 * @param id The id of the object.
+			 * @returns True if the object exists in the pool, otherwise false.
+			 */
 			inline bool Exists(Id id) {
 				return (mIndexTable[id] != USHRT_MAX);
 			}
 
+			/**
+			 * Takes an index and returns the corresponding object id, if
+			 * the index is valid (and object exists with that index).
+			 * Performance: O(1)
+			 *
+			 * @param index - The index of the object you want to find the id of.
+			 * @returns The id of the object at the specified index, if it exists.
+			 */
 			inline Id IndexToId(Index index) {
-				return (mPackedObjects[index].second);
+				if (index < 0 || index >(mPackedObjects.size() - 1))
+					return 0; // Temporary fix. This will break anything which tries to create a 
+								// packed pool with an Id type which is not some kind of primative number type.
+				else
+					return (mPackedObjects[index].second);
 			}
 
+			/**
+			 * Sorts the packed objects and rebuilds the index table. 
+			 * This is current a very expensive operation for large datasets! 
+			 * Performance: O(n)
+			 *
+			 * @param A function pointer to the comparitor function which will determine which of 2
+			 * elements will come first. This function should return TRUE if object 1 comes before object 2
+			 * in total order. 
+			 *
+			 * Usually it is useful to pass a lambda as this parameter.
+			 * 
+			 * Example:
+			 * 
+			 * pool.Sort([](const Object& obj1, const Object& obj2) {
+			 *    return (obj1 < obj2);
+			 * }
+			 */
 			inline void Sort(std::function<bool (const Object&, const Object&)> func)
 			{
 				// Sort packed objects.
